7f0ed70f9918f4c6e58ed9debcea1bdcab9eda62
Airconix <123562065+Airconix@users.noreply.github.com>
2026-09-08 20:40:54 +0200

    Dodanie Obiektówki

---
+++ Obiektówka/CosoPracownikach/Inzynier.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosoPracownikach
+{
+    public enum Skutecznosc
+    {
+        ZADEN = 0,
+        SLABY = 1,
+        SREDNI = 2,
+        DOBRY = 3,
+        WYBITNY = 4
+    }
+
+    public class Inzynier
+    {
+        private string imie = "NULL";
+        private string nazwisko = "NULL";
+        private Skutecznosc skutecznosc = Skutecznosc.ZADEN;
+        private int LiczbaProjektow = 0;
+        public void Przyjmij(string imie, string nazwisko, Skutecznosc skutecznosc) // Zadanie 1
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.skutecznosc = skutecznosc;
+            this.LiczbaProjektow = 0; // Zadanie 3
+            Console.WriteLine($"Inzynier {imie} {nazwisko} zostal przyjety do pracy z skutecznoscia {skutecznosc} i liczba projektow 0.");
+        }
+
+        public int Buduj() // Zadanie 1
+        {
+            Console.WriteLine($"Inzynier {imie} {nazwisko} buduje projekt...");
+            int projektyDoDodania = (int)skutecznosc + 1;
+            LiczbaProjektow += projektyDoDodania;
+            return LiczbaProjektow;
+        }
+        public void Pokaz() // Zadanie 1
+        {
+            Console.WriteLine($"Inzynier {imie} {nazwisko} zrealizowal {LiczbaProjektow} projektow.");
+        }
+
+        public void Porownaj(
+               (string imie, string nazwisko, int liczbaprojektow, Skutecznosc skutecznosc) pracownik1,
+               (string imie, string nazwisko, int liczbaprojektow, Skutecznosc skutecznosc) pracownik2)
+        {
+
+            if (pracownik1.skutecznosc > pracownik2.skutecznosc)
+            {
+                Console.WriteLine($"{pracownik1.imie} {pracownik1.nazwisko} jest bardziej skuteczniejszy od {pracownik2.imie} {pracownik2.nazwisko}.");
+            }
+            else if (pracownik1.skutecznosc < pracownik2.skutecznosc)
+            {
+                Console.WriteLine($"{pracownik2.imie} {pracownik2.nazwisko} jest bardziej skuteczniejszy od {pracownik1.imie} {pracownik1.nazwisko}.");
+            }
+            else
+            {
+                if (pracownik1.liczbaprojektow > pracownik2.liczbaprojektow)
+                {
+                    Console.WriteLine($"{pracownik1.imie} {pracownik1.nazwisko} jest lepszy od {pracownik2.imie} {pracownik2.nazwisko} o {(pracownik1.liczbaprojektow) - (pracownik2.liczbaprojektow)} projektow.");
+                }
+                else if (pracownik1.liczbaprojektow < pracownik2.liczbaprojektow)
+                {
+                    Console.WriteLine($"{pracownik2.imie} {pracownik2.nazwisko} jest lepszy od {pracownik1.imie} {pracownik1.nazwisko} o {(pracownik2.liczbaprojektow) - (pracownik1.liczbaprojektow)} projektow.");
+                }
+                else
+                {
+                    Console.WriteLine("Pracownicy sa rowni.");
+                }
+            }
+        }
+
+        public Skutecznosc Awansuj() // Zadanie 3
+        {
+            if (skutecznosc < Skutecznosc.WYBITNY && LiczbaProjektow >= (int)skutecznosc*5)
+            {
+                skutecznosc++;
+                Console.WriteLine($"Inzynier {imie} {nazwisko} awansowal na skutecznosc {skutecznosc}.");
+            }
+            else
+            {
+            }
+            return skutecznosc;
+        }
+    }
+}
---
+++ Obiektówka/CosoPracownikach/Program.cs
@@ -0,0 +1,83 @@
+﻿
+/*
+ Note: Gemini z wyszukiwarki i wbudowany Github Copilot w Visual Studio ogólnie zostało zrealizowane tylko do powpowiedzenia tematyki skryptów, poprawki kodu i szybszego przepisywania go.
+ */
+
+using CosoPracownikach;
+using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosoPracownikach
+{
+
+    class Program
+    {
+        Inzynier inzynier = new Inzynier();
+        List<(string imie, string nazwisko, int liczbaprojektow, Skutecznosc skutecznosc)> pracownicy =
+            new()
+            {
+                    ("Larp", "Sahurski", 0, Skutecznosc.SLABY),
+                    ("Ballerino", "Cappucino", 0, Skutecznosc.SREDNI),
+                    ("Joffstein", "Diddcomb", 0, Skutecznosc.DOBRY),
+                    ("King", "Brokul", 0, Skutecznosc.WYBITNY)
+            };
+
+
+        public void ZaaktualizujStatystyki()
+        {
+            int Liczbainzynierow = pracownicy.Count;
+            int Wszystkieprojektyfirmy = 0;
+
+            for (int i = 0; i < pracownicy.Count; i++)
+            {
+                Wszystkieprojektyfirmy += pracownicy[i].liczbaprojektow;
+            }
+
+            Console.WriteLine($"Liczba wszystkich zrealizowanych Projektow: {Wszystkieprojektyfirmy}");
+            Console.WriteLine($"Srednia liczba projektow na inzyniera: {(double)Wszystkieprojektyfirmy / Liczbainzynierow}");
+
+            var posortowani = pracownicy
+                .OrderByDescending(x => x.liczbaprojektow)
+                .ToList();
+
+            var najlepszy = posortowani[0];
+            var drugiNajlepszy = posortowani[1];
+
+            Console.WriteLine(
+                $"Najwiecej projektow: {najlepszy.imie} {najlepszy.nazwisko} - {najlepszy.liczbaprojektow}"
+            );
+
+            Console.WriteLine(
+                $"Drugi najlepszy: {drugiNajlepszy.imie} {drugiNajlepszy.nazwisko} - {drugiNajlepszy.liczbaprojektow}"
+            );
+            Console.WriteLine(
+                $"Różnica projektów: {(najlepszy.liczbaprojektow) - (drugiNajlepszy.liczbaprojektow)}"
+            );
+        }
+
+        static void Main(string[] args) // Zadanie 2
+        {
+            Inzynier inzynier = new Inzynier();
+            Program program = new Program();
+            var pracownicy = program.pracownicy;
+
+            for (int i = 0; i < pracownicy.Count; i++) // Zadanie 3
+            {
+                inzynier.Przyjmij(pracownicy[i].imie, pracownicy[i].nazwisko, pracownicy[i].skutecznosc);
+                for (int j = 0; j < 6; j++)
+                {
+                    pracownicy[i] = (pracownicy[i].imie, pracownicy[i].nazwisko, inzynier.Buduj(), inzynier.Awansuj());
+                }
+                inzynier.Pokaz();
+            }
+            inzynier.Porownaj(pracownicy[0], pracownicy[1]);
+            inzynier.Porownaj(pracownicy[3], pracownicy[2]);
+            program.ZaaktualizujStatystyki(); // Zadanie 4
+        }
+    }
+}
+
